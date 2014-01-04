cfdd0b140ffc857b42cfd5f78ebc152b06760994
timofeysie <timofeyc@hotmail.com>
2014-01-04 21:40:19 +0900

    Transform compile time type check annotation.

---
+++ src/groovy/Annotations.groovy
@@ -0,0 +1,6 @@
+import representing.Acknowledge 
+
+Acknowledge ack = new Acknowledge()
+
+ack.salude()                //misspelling
+ack.welcome( 123 )          //wrong argument type
--- src/groovy/representing/Acknowledge.groovy
+++ src/groovy/representing/Acknowledge.groovy
@@ -1,6 +1,17 @@
 package representing
 
-class Acknowldeger 
+/*
+Runtime vs Compile time, Static vs Dynamic Add
+@groovy.transform.TypeChecked
+The compiler will report errors at compile time instead of runtime.
+
+*/
+@groovy.transform.TypeChecked
+class Acknowledge
 {
-   void acknowkledge() { println 'Hello' }
+   void Acknowkledge() { println 'Hello' }
+   
+    def salute( person ) { println "Hello ${person.name}!" }
+    def welcome( String location ) { println "Welcome to ${location.state}!" }
+
 }
