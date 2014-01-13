d9fe6300914cb0fda2e8d5144ea6b0af95f1b6f3
timofeysie <timofeyc@hotmail.com>
2014-01-13 22:25:58 +0900

    Included debugging feedback.

--- src/groovy/RegExps.groovy
+++ src/groovy/RegExps.groovy
@@ -3,7 +3,7 @@
 /*
     The input string to parse is from the output of the 'df -k' command.
     Example input:
- 
+  
         Filesystem           1K-blocks      Used Available Use% Mounted on
         /dev/sda1              4185956   1206996   2762888  31% /
         /dev/sda11            30679784  28324040    772140  98% /extra
@@ -12,23 +12,26 @@
                               10485760   6599936   3885824  63% /nfs/data_d2/dog_data
         fas6070-1-1.b:/vol/felis
                              314572800  54889600 259683200  18% /nfs/DATA-1/cat_data
-  
+   
     We want the available disk space in KB, for /nfs/data remote
     disk partitions, which is "3885824" and "259683200" in the sample
     above. Note that partitions that start out with "/nfs/data" may be
-    either upper or lower case. Capture the partition name for debugging.
-  
-    Capture the space available in KB as the number before the
+    either upper or lower case. (#2:) Capture the partition name for debugging.
+   
+    (#1:) Capture the space available in KB as the number before the
     percentage number (digits followed by '%'). The pattern match
     is relying on there being just one instance of a numeric
-    percentage (\d+%) occurring in the each usable output line.
-
+    percentage (\d+%) occuring in the each usable output line.
+ 
     The header line contains a "%", but the characters preceding it
     are non-numeric, "Use%", so it is ignored. Also, many of the lines
     we want are split across two lines, but it is only the second line
     that contains the information we want.
-
+ 
     The "(?i)" match flag indicates the pattern is case insensitive.
+
+    The extended mode (?x) allows whitespace and comments starting with "#"
+    to be embedded in the regular expression.
 */
 pattern = ~/(?i)(\d+)\s+\d+%\s+(\/nfs\/data.*)/
 
@@ -48,11 +51,66 @@ You can match "#" with "\\#" so that it's not interpreted as the beginning of a 
 What does the third example buy you? Now newlines and comments can be included. The third example (here document) above can also be written:
 // here document string converted to a regex
 // a.k.a. extended match mode 
-regex =  '''(?ix)    # comments are now allowed!
-            (\\d+)   # disk space
-            \\s+
-            \\d+%    # one or more numbers followed by "%"
-            \\s+
-            (/nfs/data.*)  # partition name'''
+regex =  '''(?ix)        # case insensitive, extended format
+            (\\d+)       # 1: The disk space we want
+            \\s+         # some whitespace
+            \\d+%        # a number followed by %
+            \\s+         # some more whitespace
+            (/nfs/data.*)    # 2: partition name'''
 pattern = ~regex
 
+/*
+    The input string to parse is from...
+    <...the rest of the header comment from above...>
+    With 'debugging = true', this prints some information to show 
+    the regular expression is working on the test data:
+*/
+boolean debugging = true
+
+if (debugging) {
+    // Test data
+    dfkOutput = '''
+Filesystem           1K-blocks      Used Available Use% Mounted on
+/dev/sda1              4185956   1206996   2762888  31% /
+/dev/sda11            30679784  28324040    772140  98% /extra
+fas3050c-1-2.b:/vol/canis
+                      10485760   6599936   3885824  63% /nfs/data_d2/dog_data
+fas6070-1-1.b:/vol/felis
+                     314572800  54889600 259683200  18% /nfs/DATA-1/cat_data
+'''
+} else {
+    // Real data
+    dfkOutput = 'df -k'.execute().text
+}
+
+long kbAvail = 0
+
+regex =  '''(?ix)      # enable case-insensitive matches, extended patterns
+            (\\d+)     # 1: The disk space we want
+            \\s+       # some whitespace
+            \\d+%      # a number followed by %
+            \\s+       # some more whitespace
+            (/nfs/data.*)  # 2: partition name'''
+
+pattern = ~regex
+matcher = pattern.matcher(dfkOutput)
+
+if (debugging) {
+    println """matcher pattern: 
+/---------------------------------\\
+${matcher.pattern()}
+\\---------------------------------/"""
+
+    println "match count=${matcher.getCount()}"
+}
+
+for (i=0; i < matcher.getCount(); i++) {
+    if (debugging) {
+        println "    text matched in matcher[${i}]: '" + matcher[i][0] + "'"
+        println "        free space in  (group 1): '" + matcher[i][1] + "'"
+        println "        partition name (group 2): '" + matcher[i][2] + "'"
+    }
+    kbAvail += matcher[i][1].toLong()
+}
+
+println "KB available=${kbAvail}"
