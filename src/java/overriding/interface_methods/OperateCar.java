afb82ba4ecf72406d5b6ac3d17192356ea305c04
timofeysie <timofeyc@hotmail.com>
2014-01-16 20:11:01 +0900

    Added flying car and LocalDateTime that requires Java 8.

---
+++ src/java/overriding/interface_methods/OperateCar.java
@@ -0,0 +1,26 @@
+package overriding.interface_methods;
+
+public interface OperateCar 
+{
+
+   // constant declarations, if any
+
+   // method signatures
+   
+   // An enum with values RIGHT, LEFT
+   int turn(Direction direction,
+            double radius,
+            double startSpeed,
+            double endSpeed);
+   int changeLanes(Direction direction,
+                   double startSpeed,
+                   double endSpeed);
+   int signalTurn(Direction direction,
+                  boolean signalOn);
+   int getRadarFront(double distanceToCar,
+                     double speedOfCar);
+   int getRadarRear(double distanceToCar,
+                    double speedOfCar);
+         //......
+   // more method signatures
+}
