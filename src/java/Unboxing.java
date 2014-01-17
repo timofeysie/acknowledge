643919b212a53071ba836346bb610b2d3acd531c
timofeysie <timofeyc@hotmail.com>
2014-01-17 13:20:17 +0900

    Added boxing and unboxing classes.

---
+++ src/java/Unboxing.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Unboxing 
+{
+
+    public static void main(String[] args) 
+    {
+
+        // First, Auto-Boxing
+        List<Integer> auto_boxing_li = new ArrayList<>();
+        for (int i = 1; i < 50; i += 2)
+            auto_boxing_li.add(i);
+        System.out.println("Auto-Boxing, int to Integer: "+auto_boxing_li.get(1));
+
+        // equivalent to:
+        List<Integer> manual_boxing_li = new ArrayList<>();
+        for (int i = 1; i < 50; i += 2)
+            manual_boxing_li.add(Integer.valueOf(i));
+        System.out.println("Manual-Boxing "+manual_boxing_li.get(1));
+        
+        // Now the Unboxing
+        Integer i = new Integer(-8);
+        // 1. Unboxing through method invocation
+        int absVal = absoluteValue(i);
+        System.out.println("absolute value of " + i + " = " + absVal);
+
+        List<Double> ld = new ArrayList<>();
+        ld.add(3.1416);    // (pi) is autoboxed through method invocation.
+
+        // 2. Unboxing through assignment
+        double phi = ld.get(0);
+        System.out.println("pi is autoboxed through method invocation: " + phi);
+    }
+
+    public static int absoluteValue(int i) 
+    {
+        return (i < 0) ? -i : i;
+    }
+
+}
