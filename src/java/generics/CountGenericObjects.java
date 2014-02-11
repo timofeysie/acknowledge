7bc5392b16ba359ca7d0a662c2fb02cce19f0123
timofeysie <timofeyc@hotmail.com>
2014-02-11 09:48:13 +0900

    Added code to generic object count class.

--- src/java/generics/CountGenericObjects.java
+++ src/java/generics/CountGenericObjects.java
@@ -18,6 +18,22 @@ public class CountGenericObjects
         return count;
     }
 
+    public static <E> void exchangeElements(E[]array, E elem1, E elem2)
+    {
+        int index = 0;
+        int position_1 = 0;
+        int position_2 = 0;
+        for (E element : array)
+        {      
+            if (element.equals(elem1))
+                position_1 = index;
+            else if (element.equals(elem2))
+                position_2 = index;
+        }
+        array[position_1] = elem2; // ArrayStoreException
+        array[position_2] = elem1;
+    }
+
     public static void main( String args[] )
     {
         // Create arrays of Integer, Double and Character
@@ -31,13 +47,23 @@ public class CountGenericObjects
         System.out.println( "\nArray doubleArray contains:" );
         printArray( doubleArray ); // pass a Double array
 
+        System.out.println( "\nAfter switch:" );
+        exchangeElements(doubleArray, 1.1, 4.4);
+        printArray( doubleArray ); // pass a Double array
+
         System.out.println( "\nArray characterArray contains:" );
         printArray( charArray ); // pass a Character array
-        int count = Integer.valueOf(args[0]);
+        int count = 5;
+        if (args.length > 0)
+            count = Integer.valueOf(args[0]);
         int counted = countElements(intArray, count);
         String ending = ".";
         if (counted>1)
             ending = "s."; 
         System.out.println( "Array integerArray contains "+countElements(intArray, count)+" "+count+ending);
+    
+        System.out.println("\nAfter switch:");
+        exchangeElements(charArray, "H", "O");
+        printArray(charArray); // pass a Double array
     } 
 }
