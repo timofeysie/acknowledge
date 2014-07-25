d207562a9505cfb5061c02ec5fc26e779bacb3f4
timofeysie <timofeyc@hotmail.com>
2014-07-26 09:24:43 +1000

    Added a Test35 from Three page 4-5.

---
+++ src/java/exam/Test35.java
@@ -0,0 +1,12 @@
+class Test35A{
+     public Test35A(int i){
+     	System.out.println("No no arguments constructor");
+     }
+}
+
+class Test35 extends Test35A{
+}
+	public static void main(String[] args) {
+		new B(10);
+	}
+}
